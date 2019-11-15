4b6faa7e62734e8b0c207279f49034726388645b
Kyle Barnett <kbarnett23@gmail.com>
2019-11-14 21:37:08 -0700

    POST working too

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   db('cars')
-  .then(results => {
-    res.json(results)
-  })
+    .then(results => {
+      res.json(results)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error getting car data.' })
+    })
 })
+
+server.post('/', (req, res) => {
+  const { body } = req;
+  db('cars').insert(body)
+    .then(car => {
+      res.status(201).json(car)
+    })
+})
+
 module.exports = server;
