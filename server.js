02420ae57b4abc9205028019d8c0e07bb1105689
Kyle Barnett <kbarnett23@gmail.com>
2019-11-14 21:41:42 -0700

    MVP

--- server.js
+++ server.js
@@ -15,11 +15,29 @@ server.get('/', (req, res) => {
     })
 })
 
+server.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('cars').where({ id })
+    .then(car => {
+      if (car.length > 0) {
+        res.status(200).json(car)
+      } else {
+        res.status(404).json({ message: `${id} not found.` })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error getting ID.' })
+    })
+})
+
 server.post('/', (req, res) => {
   const { body } = req;
   db('cars').insert(body)
     .then(car => {
       res.status(201).json(car)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error creating car entry.' })
     })
 })
 
